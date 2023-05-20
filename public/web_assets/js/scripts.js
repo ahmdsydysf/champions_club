2a78be7b9a712d07c6d5ca6f72fe7f81af373ea9
sabreenMohammed107 <sabreenm312@gmail.com>
2023-05-20 12:19:15 +0200

    20-5-2023

--- public/web_assets/js/scripts.js
+++ public/web_assets/js/scripts.js
@@ -327,7 +327,7 @@
 
             $(".owl-carousel").owlCarousel({
                 autoPlay: 3000,
-                items : 1, // THIS IS IMPORTANT
+                items : 3, // THIS IS IMPORTANT
                 responsive : {
                       480 : { items : 1  }, // from zero to 480 screen width 4 items
                       768 : { items : 2  }, // from 480 screen widthto 768 6 items
