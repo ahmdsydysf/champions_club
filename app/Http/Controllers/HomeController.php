eef2f087f6d4e130378b2bb1542770f8dd10a53a
sabreenMohammed107 <sabreenm312@gmail.com>
2023-05-21 00:07:40 +0200

    migration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Models\Attendance;
+use App\Models\User_membership;
 use Illuminate\Support\Facades\Auth;
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Session;
@@ -122,19 +123,14 @@ class HomeController extends Controller
         $sportDays = Sports_day::where('id', $sport_id)->firstOrFail();
         $firstday_name = Day_new::where('id', $sportDays->firstday_id)->firstOrFail();
         $secondday_name =Day_new::where('id', $sportDays->secondday_id)->firstOrFail();
-
-
-
-
-
-         $annual=null;
-        $annual=App\Models\User_membership::where('user_id',$child->user_id)->
+$annual=null;
+        $annual=User_membership::where('user_id',Auth::user()->id)->
         where('end_date','>',now()->format('Y-m-d'))->where('approved',1)->first();
 
 
         if ($annual !== null){
            $member= $SportData[0]->membership_disc_fees;
-        }else
+        }else{
         $member=$SportData[0]->membership_fees;
      }
 
