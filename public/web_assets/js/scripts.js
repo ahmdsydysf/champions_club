0efd25a9476714fab2803a8b26201db68f084dcf
sabreenMohammed107 <sabreenm312@gmail.com>
2023-05-20 12:22:20 +0200

    20-5-2023

--- public/web_assets/js/scripts.js
+++ public/web_assets/js/scripts.js
@@ -329,6 +329,7 @@
                 loop: true,
                 autoplay: true,
                 items : 3, // THIS IS IMPORTANT
+                autoplayTimeout: 3200,
                 responsive : {
                       480 : { items : 1  }, // from zero to 480 screen width 4 items
                       768 : { items : 2  }, // from 480 screen widthto 768 6 items
