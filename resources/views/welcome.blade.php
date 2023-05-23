1e531794282c5ef23facf60d1dc83257fcf96fdd
sabreenMohammed107 <sabreenm312@gmail.com>
2023-05-23 02:14:10 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -197,20 +197,20 @@
 
             </div>
 
-            <div id="owl-en" class="owl-carousel">
+            {{-- <div id="owl-en" class="owl-carousel">
                 @foreach ($services as $row)
-                {{-- <div class="content-box service-container"> --}}
+                <div class="content-box service-container">
 
                     <figure><img src="{{ asset('uploads/service/' . $row->image) }}" alt="Image"></figure>
                     <h4 style="color:#fff">{{ $row->title_en }}</h4>
                     <p style="color:#fff">{{ $row->brief_en }}</p>
                     <a class="dis-btn-service" href="#" data-swiper-parallax="200">Discover
                         More<span></span></a>
-                {{-- </div> --}}
+                </div>
                 @endforeach
 
 
-            </div>
+            </div> --}}
 
         </div>
 
